954e273891f986c43a2638d3d94314d479b9ea00
lidijasuprina <lidija.suprina@creative-square.agency>
2022-11-30 00:35:59 +0100

    Creating first user with database seeds

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,8 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('first_name');
-            $table->string('last_name');
+            $table->string('first_name')->nullable(true);
+            $table->string('last_name')->nullable(true);
             $table->string('email', 100)->unique();
             $table->string('password');
         });
